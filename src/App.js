edbe64e39f77a91a9630968b0b4050e2196bd2fb
Krisztian Bui <buikrisz00@gmail.com>
2022-05-13 17:24:39 +0200

    a

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Ozone from './components/OzoneComponents/Ozone/Ozone';
 
 function App() {
     return (
-        <BrowserRouter>
+        /* <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <PestControl /> }/>
                 <Route path="/ozone" element={ <Ozone /> }/>
             </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */
+    
+        <PestControl />
+    
+
     );
 }
 
